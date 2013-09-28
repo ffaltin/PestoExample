786e1170bc5e5e7fba312a8f5829a431e0e466c4
Fred Faltin <ffaltin@camelidae.be>
2013-09-28 10:54:54 +0200

    add example

--- app/init.php
+++ app/init.php
@@ -12,61 +12,82 @@
 	
 	require __DIR__.'/autoload.php';
 	
+	class MarvelousController extends Pesto\Handling\Controller {
+		public function indexAction() {
+			$v = $this->getRepo("person")->getOneBy("name","Mon bateau");
+			return $this->createView('public/hello',[
+				'hello' => "Mon dieu",
+				'title' => "Title",
+			],[
+				'title' => 'Mon beau titre',
+			]);
+		}
+	}
+	
 	$Application = new Pesto\Application();
 	$Application
 		->defineDatabase(new Pesto\Storage\Database("ffaltin","ff","www_testcom"))
 		->setConfig([
 			"pathApp" => __DIR__,
 			"baseLayoutName" => "base",
+			"themeName" => "desktop",
+			"appName" => "Mon petit bateau rouge",
 		])
 	;
+	
+	$marvelousController = new MarvelousController();
+	$marvelousController->defineApplication($Application);
+	
 	// fast way
-	$Application->match('/', function() use ($Application){
-		$view = $Application->createView("public/hello")
-			->assign([
-				"hello"=>"Hello ds world",
-			])
-			->render()
-		;
-		
-		// If you want to use layout
-		return $Application->getLayout()->assign([
-			"content"=>$view,
-			"title" => "Hello",
-			"hello" => "Mondieu",
-		]);
-	});
-
-	// controllers way
-	$Application->match("/test/(\d+)", "index.hello");
+	$Application
+		->match('/', function() use ($Application){
+			$view = $Application->createView("public/hello")
+				->assign([
+					"hello"=>"Hello ds world",
+				])
+				->render()
+			;
+			
+			// If you want to use layout
+			return $Application->getLayout()->assign([
+				"content"=>$view,
+				"title" => "Hello",
+				"hello" => "Mondieu",
+			]);
+		})
+		// controllers way (external file)
+		->match("/(\d+)", "index.hello")
+		// controllers way (inside file)
+		->match("/marvelous", $marvelousController->indexAction())
 	// Mount routes
-	$Application->mount("/hello",function() use ($Application) {
-		$Application->get("/Jhon",function() use ($Application) {
-			print $Application->createView("public/hello")
-				->assign([
-					"hello"=>"Hello het",
-				])
-				->render()
-			;
-		});
-		$Application->get("/het",function() use ($Application) {
-			print $Application->createView("public/hello")
-				->assign([
-					"hello"=>"Hello het",
-				])
-				->render()
-			;
-		});
-		
-		$Application->get("/([a-z0-9]+)",function($name) use ($Application) {
-			print $Application->createView("public/hello")
-				->assign([
-					"hello"=>$name,
-				])
-				->render()
-			;
-		});
-	});
+		->mount("/hello",function() use ($Application) {
+			$Application->get("/Jhon",function() use ($Application) {
+				print $Application->createView("public/hello")
+					->assign([
+						"hello"=>"Hello het",
+					])
+					->render()
+				;
+			});
+			$Application->get("/het",function() use ($Application) {
+				print $Application->createView("public/hello")
+					->assign([
+						"hello"=>"Hello het",
+					])
+					->render()
+				;
+			});
+			
+			$Application->get("/([a-z0-9]+)",function($name) use ($Application) {
+				print $Application->createView("public/hello")
+					->assign([
+						"hello"=>$name,
+					])
+					->render()
+				;
+			});
+		})
+	;
 	
 	$Application->run();
 	
--- app/src/App/Controller/IndexController.php
+++ app/src/App/Controller/IndexController.php
@@ -5,7 +5,7 @@
 	class IndexController extends \Pesto\Handling\Controller {
 		
 		public function helloAction($args) {
-			$v = $this->application->getRepository("person")->getOneBy("name","Mon bateau");
+			$v = $this->getRepo("person")->getOneBy("name","Mon bateau");
 			return $this->createView('public/hello',[
 				'hello' => "Mon dieu",
 				'title' => "Title",
